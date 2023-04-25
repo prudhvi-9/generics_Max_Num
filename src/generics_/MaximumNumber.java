ed6e4f77985d54eadc73bb0128365e39113d2035
PRUDHVI-9 <prudhvirajathelli@gmail.com>
2023-04-25 09:38:37 +0530

    Added:Find maximum number.

---
+++ src/generics_/MaximumNumber.java
@@ -0,0 +1,81 @@
+package generics_;
+
+import java.util.Scanner;
+
+public class MaximumNumber<T extends Comparable<T>> {
+
+	T a, b, c;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 */
+	public MaximumNumber(T a, T b, T c) {
+
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public T maximum() {
+		return MaximumNumber.maximum(a, b, c);
+	}
+
+	/**
+	 * To print the data.
+	 * 
+	 * @param <T>
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @param max
+	 */
+	public static <T> void printMax(T a, T b, T c, T max) {
+		System.out.printf("max of %s, %s and %s is %s\n", a, b, c, max);
+
+	}
+
+	/**
+	 * Comparing a,b,c to find maximum.
+	 * 
+	 * @param <T>
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 */
+	public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
+		T max = a;
+		if (b.compareTo(max) > 0) {
+			max = b;
+		}
+		if (c.compareTo(max) > 0) {
+			max = c;
+		}
+		printMax(a, b, c, max);
+		return max;
+	}
+
+	/**
+	 * This method is created for finding the maximum number.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner cs = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		System.out.println("Enter value of a:");
+		Integer aInt = cs.nextInt();
+		System.out.println("Enter value of b:");
+		Integer bInt = cs.nextInt();
+		System.out.println("Enter value of c:");
+		Integer cInt = cs.nextInt();
+
+		new MaximumNumber(aInt, bInt, cInt).maximum();
+		cs.close();
+	}
+
+}
